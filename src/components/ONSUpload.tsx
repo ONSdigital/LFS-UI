fd0f9dd07c2fbde1d3db444f4c48fc4f239b0e82
Matthew Palmer <palmem2@Matthews-MacBook-Pro.local>
2019-10-10 12:40:15 +0100

    Fixed Text Spacing

--- src/components/ONSUpload.tsx
+++ src/components/ONSUpload.tsx
@@ -14,9 +14,10 @@ export class ONSUpload extends Component <Props, {}>{
       return (
         <div className="field">
             <p className="field">
-                <label className="label" >{this.props.label}</label>
-                <span className="label__description">{this.props.description}</span>
-                
+                <label className="label" >{this.props.label}
+                <br/>
+                  <span className="label__description">{this.props.description}</span>
+                </label>
                 <input type="file" id={this.props.fileID} className="input input--text input-type__input input--upload" name={this.props.fileName} accept={this.props.accept} />
             </p>
         </div>
