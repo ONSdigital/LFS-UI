b499a28dc5c077f0bb581437e6b2fc974b715382
Matthew Palmer <palmem2@Matthews-MacBook-Pro.local>
2019-10-10 11:30:57 +0100

    Removed Broken Error Pannel

--- src/components/ONSPanel.tsx
+++ src/components/ONSPanel.tsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 interface Props{
   label: string,
   children: any,
-  header?: string,
   status? : string,
   spacious?: boolean,
   id?: string
@@ -15,11 +14,6 @@ export class ONSPanel extends Component <Props, {}>{
       let className = "panel panel--" + (this.props.status === "success" ? "success" : this.props.status === "error" ? "error": "info") + " panel--simple " + (this.props.spacious ? "panel--spacious" : "");
       return (
         <div id={this.props.id} className={className}>
-          {this.props.header !== undefined &&
-            <div className="panel__header">
-              <h1 className="panel__title u-fs-r--b">{this.props.header}</h1>
-            </div>
-          }
           <div className="panel__body">
               {this.props.children}
             </div>
