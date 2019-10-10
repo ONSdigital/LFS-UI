50034839c311b30d204d4a2403dd8031693163a5
Matthew Palmer <palmem2@Matthews-MacBook-Pro.local>
2019-10-10 11:06:32 +0100

    Added Panels to Dashboard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 ﻿import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
+import { ONSPanel } from '../components/ONSPanel';
+import { ONSTextInput } from '../components/ONSTextInput';
+import { ONSInDevBanner } from '../components/ONSInDevBanner';
+import { ONSUpload } from '../components/ONSUpload';
 
 export class Home extends Component{
   displayName = Home.name
@@ -9,7 +13,26 @@ export class Home extends Component{
 
   render() {
     return (
-      <p>This is the Dashboard</p>
+      <div className="container">
+        <p>Hello</p>
+        <ONSPanel label="This is the Dashboard" status="info" spacious={true}>
+          <p>This is the Dashboard</p>
+        </ONSPanel>
+        <ONSPanel label="You got to the Dashboard" status="success">
+          <p>You got to the Dashboard</p>
+        </ONSPanel>
+        <ONSPanel label="" status="error">
+          <p>Nothing Loaded</p>
+        </ONSPanel>
+        <ONSPanel label="" status="error" header="Oh No">
+          <p>Nothing Loaded</p>
+        </ONSPanel>
+
+        <ONSTextInput label="Text Input" placeholder="Type here ...." />
+
+        <ONSUpload label="Upload CSV" description="File types accepted are .csv" fileName="file" fileID="file"/> 
+      </div>
+      
     );
   }
 }
