1cafb34bebd40d893510e1a34ff78ecfe96175fa
palmem2 <matthewandrewpalmer@outlook.com>
2019-10-14 16:31:23 +0100

    Added Hidden prop

--- src/components/ONSPanel.tsx
+++ src/components/ONSPanel.tsx
@@ -5,7 +5,8 @@ interface Props{
   children: any,
   status? : string,
   spacious?: boolean,
-  id?: string
+  id?: string,
+  hidden?: boolean
 }
 
 export class ONSPanel extends Component <Props, {}>{
@@ -13,7 +14,7 @@ export class ONSPanel extends Component <Props, {}>{
     render() {
       let className = "panel panel--" + (this.props.status === "success" ? "success" : this.props.status === "error" ? "error": "info") + " panel--simple " + (this.props.spacious ? "panel--spacious" : "");
       return (
-        <div id={this.props.id} className={className}>
+        <div id={this.props.id} className={className} hidden={this.props.hidden}>
           <div className="panel__body">
               {this.props.children}
             </div>
