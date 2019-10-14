0ce11eb72d8a5101c5a9d22923fc266643e7892c
palmem2 <matthewandrewpalmer@outlook.com>
2019-10-14 09:09:06 +0100

    onChange Method added to Upload Component

--- src/components/ONSUpload.tsx
+++ src/components/ONSUpload.tsx
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React, {ChangeEvent, Component} from 'react';
 
 interface Props{
   label: string,
   description: string,
   fileName: string,
   fileID: string,
-  accept: string
+  accept: string,
+  onChange? : (e: ChangeEvent<HTMLInputElement>, ...args: any[]) => void
 }
 
 export class ONSUpload extends Component <Props, {}>{
+
+    value: string = "";
+    constructor(props : Props) {
+        super(props);
+        this.state = {value: ""};
+    }
+
+    handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if(this.props.onChange !== undefined){
+            this.props.onChange(e, this.props.label);
+        }
+        this.value = e.target.value;
+
+    };
 
     render() {
       return (
@@ -18,7 +33,7 @@ export class ONSUpload extends Component <Props, {}>{
                 <br/>
                   <span className="label__description">{this.props.description}</span>
                 </label>
-                <input type="file" id={this.props.fileID} className="input input--text input-type__input input--upload" name={this.props.fileName} accept={this.props.accept} />
+                <input type="file" id={this.props.fileID} className="input input--text input-type__input input--upload" name={this.props.fileName} accept={this.props.accept} onChange={(e) => this.handleChange(e)}/>
             </p>
         </div>
       );
