1a8090175f048501c5f821c127c3007234ebf8ce
elthorne <elinor.thorne@ext.ons.gov.uk>
2019-10-18 14:57:57 +0100

    User authorisation modified.

--- src/auth/auth.tsx
+++ src/auth/auth.tsx
@@ -1,20 +1,30 @@
 let users: any[] = []
 
 function loginUser(username: string, password: string) {
-
     const request = async () => {
-        const res = await fetch("http://127.0.0.1:8000/login/" + username);
+
+        const res = await fetch("/login/" + username, {
+            "headers":{
+                "password": password
+            }
+        });
         const data = await res.json();
 
-        let user = {
-            name: "Admin",
-            role: {
-                name: "Admin",
-                pages: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-            }
-        };
-        return user
+        if(data.status === "OK"){
+            let user = {
+                name: username,
+                password: password,
+                //TODO: Role functionality to be removed
+                role: {
+                    name: "Admin",
+                    pages: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                }
+            };
+            return user
+        }
+        return undefined;
     };
+
     return request()
 }
 
