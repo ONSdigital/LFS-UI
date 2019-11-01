e305147bfb68329561da1c4b15fdfcbbfb394627
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-11-01 10:10:32 +0000

    Changed ONSRadioButton and ONSCheckbox to simpler Functions

--- src/components/ONSRadioButton.tsx
+++ src/components/ONSRadioButton.tsx
@@ -1,4 +1,4 @@
-import React, {Component, ChangeEvent, CSSProperties} from 'react';
+import React, {CSSProperties} from 'react';
 
 interface Props {
     label: string,
@@ -8,53 +8,21 @@ interface Props {
     style?: CSSProperties
 }
 
-interface State {
-    value: boolean
-}
-
-export class ONSRadioButton extends Component <Props, State> {
-    value: boolean = this.props.checked === true;
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {value: props.checked === true};
-    }
-
-    handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (this.props.onChange !== undefined) {
-            this.props.onChange(e);
-        }
-        this.value = e.target.checked;
-        this.setState({value: e.target.checked})
-
-    };
-
-    static getDerivedStateFromProps(nextProps: Props) {
-        if (nextProps.checked !== undefined) {
-            return {value: nextProps.checked};
-        } else return null;
-    }
-
-    render() {
-        return (
-            <p className="radios__item" style={this.props.style}>
+export const ONSRadioButton = (props: Props) => (
+    <p className="radios__item" style={props.style}>
                 <span className="radio">
                     <input
                         type="radio"
-                        id={this.props.id}
+                        id={props.id}
                         className={"radio__input js-radio"}
-                        checked={this.state.value}
-                        onChange={(e) => this.handleChange(e)}
-                        value={this.props.id}
-                        name={this.props.label}
+                        checked={props.checked}
+                        onChange={(e) => props.onChange ? props.onChange(e) : e}
+                        value={props.id}
+                        name={props.label}
                     />
-                    <label className="radio__label " htmlFor={this.props.id}>
-                        {this.props.label}
+                    <label className="radio__label " htmlFor={props.id}>
+                        {props.label}
                     </label>
                 </span>
-            </p>
-        );
-    }
-}
-
-
+    </p>
+);
