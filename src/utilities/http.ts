1cc0e612e1f033a09bb2343c6ef551e5e4117a4b
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-11-04 16:53:01 +0000

    HTTP Requests

--- src/utilities/http.ts
+++ src/utilities/http.ts
@@ -21,3 +21,49 @@ export function postFile(lfsfile: any, fileName: string, fileType: string, fileS
             });
     })
 }
+
+export function createNewBatch(batchType: string, year: string, period: string, description: string): Promise<any> {
+
+    let formData = new FormData();
+    formData.append("description", description);
+
+    let url = "/batches/" + batchType + "/" + year  + (batchType !== 'annually' ? "/" + period : "");
+
+    return new Promise((resolve: any, reject: any) => {
+        fetch(url, {
+            "method": "POST",
+            "body": formData,
+        })
+            .then(response => {
+                console.log("Response");
+                console.log(response);
+                resolve(response.json());
+            })
+            .catch(err => {
+                console.log("Error");
+                console.log(err);
+                reject(err)
+            });
+    })
+}
+
+export function getBatch(batchType: string, year: string, period: string): Promise<any> {
+
+    let url = "/batches/display/" + batchType + "/" + year + (batchType !== 'annually' ? "/" + period : "");
+
+    return new Promise((resolve: any, reject: any) => {
+        fetch(url, {
+            "method": "GET"
+        })
+            .then(response => {
+                console.log("Response");
+                console.log(response);
+                resolve(response.json());
+            })
+            .catch(err => {
+                console.log("Error");
+                console.log(err);
+                reject(err)
+            });
+    })
+}
