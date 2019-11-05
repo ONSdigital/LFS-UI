dbc09eabc3297805fbc127b0a6267f9183d96ccc
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-11-05 14:10:46 +0000

    Fixed Metadata key error

--- src/components/ONSMetadata.tsx
+++ src/components/ONSMetadata.tsx
@@ -11,7 +11,7 @@ export class ONSMetadata extends Component < Props, {} > {
         return(
             <div>
                 <dl className="metadata metadata__list grid grid--gutterless u-cf u-mb-l" title="This is an example of the metadata component" aria-label="This is an example of the metadata component">
-                    {this.props.List.map((item:any) => (<div>
+                    {this.props.List.map((item:any) => (<div key={item.L}>
                                                             <dt className="metadata__term grid__col col-2@m">{item.L}:</dt>  
                                                             <dd className="metadata__value grid__col col-10@m">{item.R}</dd>
                                                         </div>)
