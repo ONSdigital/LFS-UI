374994ab3002ae5ec046c74a32873b18b16083d8
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-11-07 09:46:03 +0000

    Added Show Password to Password Input

--- src/components/ONSPasswordInput.tsx
+++ src/components/ONSPasswordInput.tsx
@@ -1,24 +1,58 @@
-import React, { Component } from 'react';
+import React, {ChangeEvent, Component} from 'react';
 
-interface Props{
-  label?: string,
-  id?: string,
-  placeholder? : string
+interface Props {
+    label?: string,
+    id?: string,
+    placeholder?: string,
+    onChange?: (e: ChangeEvent<HTMLInputElement>, ...args: any[]) => void,
 }
 
-export class ONSPasswordInput extends Component <Props, {}>{
-  constructor(props : Props) {
-    super(props);
-  }
+interface State {
+    password: boolean
+}
+
+export class ONSPasswordInput extends Component <Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {password: true};
+    }
+
+    togglePassword = () => {
+        this.setState({password: !this.state.password})
+    };
+
+    handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (this.props.onChange !== undefined) {
+            this.props.onChange(e, this.props.label);
+        }
+    };
+
 
     render() {
-    return (
-      <p className="field">
-        {this.props.label !== undefined &&
-        <label className="label">{this.props.label}</label>
-        }
-        <input type="password" id={this.props.id} className="input input--text input-type__input " placeholder={this.props.placeholder} />
-      </p>
-    );
-  }
+        return (
+            <p className="field">
+                <label className="label" htmlFor="password">Password</label>
+                <span className="checkbox checkbox--toggle">
+            <input
+                type="checkbox"
+                id="password-toggle"
+                className="checkbox__input"
+                value=""
+                name="show-password"
+                onClick={this.togglePassword}
+            />
+            <label id="password-toggle-label" className="checkbox__label " htmlFor="password-toggle">
+                Show password
+            </label>
+          </span>
+                <label className="label" htmlFor="password"/>
+                <input
+                    type={this.state.password ? "password" : "text"} id="password"
+                    className="input input--text input-type__input u-mt-xs"
+                    onChange={(e) => this.handleChange(e)}
+                />
+            </p>
+
+        );
+    }
 }
