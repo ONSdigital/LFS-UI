9d03e0af44a1aab8697badd06eeac953f85c446a
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-11-14 14:30:42 +0000

    removed repeating Request Code

--- src/utilities/http.ts
+++ src/utilities/http.ts
@@ -1,117 +1,51 @@
 function postSurveyFile(lfsfile: any, fileName: string, fileType: string, survey: string, period: string, year: string): Promise<any> {
-
+    let url = "/imports/" + fileType + "/" + survey + "/" +  year + "/" +  period;
     let formData = new FormData();
     formData.append("lfsFile", lfsfile[0]);
     formData.append("fileName", fileName);
     formData.append("fileSource", survey);
 
-    return new Promise((resolve: any, reject: any) => {
-        fetch("/imports/" + fileType + "/" + survey + "/" +  period + "/" +  year, {
-            "method": "POST",
-            "body": formData,
-        })
-            .then(response => {
-                console.log(response);
-                resolve(response.json());
-            })
-            .catch(err => {
-                console.log(err);
-                reject(err)
-            });
-    })
+    return genericRequest("POST", url, formData);
 }
 
 function postImportFile(importFile: any, link :string, fileName: string): Promise<any> {
-    console.log("/imports/" + fileName + "-----------------------");
+    let url = "/imports/" + link;
     let formData = new FormData();
     formData.append("lfsFile", importFile[0]);
     formData.append("fileName", fileName);
 
-    return new Promise((resolve: any, reject: any) => {
-        fetch("/imports/" + link, {
-            "method": "POST",
-            "body": formData,
-        })
-            .then(response => {
-                console.log(response);
-                resolve(response.json());
-            })
-            .catch(err => {
-                console.log(err);
-                reject(err)
-            });
-    })
+    return genericRequest("POST", url, formData);
 }
 
 function createNewBatch(batchType: string, year: string, period: string, description: string): Promise<any> {
-
     let formData = new FormData();
     formData.append("description", description);
-
     let url = "/batches/" + batchType + "/" + year + (batchType !== 'annually' ? "/" + period : "");
 
-    return new Promise((resolve: any, reject: any) => {
-        fetch(url, {
-            "method": "POST",
-            "body": formData,
-        })
-            .then(response => {
-                console.log("Response");
-                console.log(response);
-                resolve(response.json());
-            })
-            .catch(err => {
-                console.log("Error");
-                console.log(err);
-                reject(err)
-            });
-    })
+    return genericRequest("POST", url, formData);
 }
 
 function getBatchData(batchType: string, year: string, period: string): Promise<any> {
     let url = "/batches/display/" + batchType + "/" + year + (batchType !== 'annually' ? "/" + period : "");
-
-    return new Promise((resolve: any, reject: any) => {
-        fetch(url, {
-            "method": "GET"
-        })
-            .then(response => {
-                console.log("Response");
-                console.log(response);
-                resolve(response.json());
-            })
-            .catch(err => {
-                console.log("Error");
-                console.log(err);
-                reject(err)
-            });
-    })
+    return genericRequest("GET",url);
 }
 
 function getAllBatches(): Promise<any> {
-    return new Promise((resolve: any, reject: any) => {
-        fetch("/dashboard", {
-            "method": "GET"
-        })
-            .then(response => {
-                console.log("Response");
-                console.log(response);
-                resolve(response.json());
-            })
-            .catch(err => {
-                console.log("Error");
-                console.log(err);
-                reject(err)
-            });
-    })
+    let url = "/dashboard";
+    return genericRequest("GET",url);
 }
 
 function getSurveyAudit(survey: string, year: string, period: string): Promise<any> {
     let url = "/audits/" + (survey === 'gb' ? 'week' : 'month') + "/" + year + "/" + period;
+    return genericRequest("GET", url)
+}
 
+
+function genericRequest(method: string, url: string, body: any = null): Promise<any> {
     return new Promise((resolve: any, reject: any) => {
         fetch(url, {
-            "method": "GET"
+            "method": method,
+            "body": (body !== null ? body : body)
         })
             .then(response => {
                 console.log("Response");
