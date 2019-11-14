d4c65f19b3bc06a0b4a354c94ac5cffd5ad3a113
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-11-14 14:56:06 +0000

    Added itemCursorHover to Table Row with expandable table set to true

--- src/components/ONS_DesignSystem/ONSAccordionTable.tsx
+++ src/components/ONS_DesignSystem/ONSAccordionTable.tsx
@@ -92,7 +92,7 @@ export class ONSAccordionTable extends Component <Props, State> {
                             this.state.data !== null && this.state.data.length !== 0 ?
                                 this.state.slicedData.map((row: DashboardTableRow, index: number) =>
                                     <Fragment key={(this.props.expandedRowEnabled ? row.id : index)}>
-                                        <tr className="table__row selectableTableRow"
+                                        <tr className={("table__row selectableTableRow " + (this.props.expandedRowEnabled ? "itemCursorHover " : ""))}
                                             onClick={((e) => this.handleClickOnRow(e, row, index))}
                                             tabIndex={0}
                                             onKeyPress={((e => this.handleEnterKeyPressOnRow(e, row, index)))}>
