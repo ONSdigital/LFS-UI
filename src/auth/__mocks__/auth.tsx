d89085ad3654f00f5a04ada2123f49d9eefbfc7a
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-12-20 16:53:13 +0000

    Tests for the Login Page - All Pass 100% Coverage

--- src/auth/__mocks__/auth.tsx
+++ src/auth/__mocks__/auth.tsx
@@ -1,11 +1,21 @@
 import {Cookies} from "react-cookie";
 
 function loginUser(username: string, password: string, cookies: Cookies) {
-    return {
-        name: "user"
+    const request = () => {
+        console.log("AUTH MOCK - Username: " + username + " Password : " + password);
+        if (username === "Admin" && password === "password") {
+            cookies.set("username", username, {path: "/", expires: new Date(), sameSite: true});
+            return {
+                name: "Admin"
+            };
+        } else {
+            return null;
+        }
     };
+
+    return request();
 }
 
 export {
     loginUser
-}
+};
