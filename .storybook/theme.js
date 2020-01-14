2e638f89aa7a6acbec83e4df72dffe32de15698b
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-01-14 14:33:39 +0000

    storybook for ONSProgressBar

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import {create} from "@storybook/theming/create";
+
+export default create({
+    base: 'light',
+
+    brandTitle: 'ONS Design System React Components',
+    brandUrl: 'https://example.com',
+    brandImage: 'https://cdn.ons.gov.uk/sdc/design-system/[VERSION]/favicons/favicon-32x32',
+});
