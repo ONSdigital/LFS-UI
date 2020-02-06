1e4a3cf8b984541c45b62c37f8f88fd58793224b
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-02-06 14:27:03 +0000

    Fixed issue with dual export of lists

--- src/tests/setup/__mocks__/mock_data.js
+++ src/tests/setup/__mocks__/mock_data.js
@@ -1,11 +1,11 @@
-export const variableDefinitionData = [
+const variableDefinitionData = [
     {id: 1, variable: "IOUTCOME", label: {String: "LABELS0", Valid: true}, description: {Valid: false}, type: "double", length: 8, precision: 0, alias: {String: "", Valid: false}, editable: false, imputation: false, dv: false, validFrom: "2019-12-18T15:14:49.306583Z"},
     {id: 2, variable: "IOUTCOME", label: {String: "LABELS0", Valid: true}, description: {String: "Previous Definition description", Valid: true}, type: "double", length: 8, precision: 0, alias: {String: "", Valid: false}, editable: false, imputation: false, dv: false, validFrom: "2018-12-18T15:14:49.306583Z"},
     {id: 3, variable: "IOUTCOME", label: {String: "LABELS0", Valid: true}, description: {Valid: false}, type: "double", length: 8, precision: 0, alias: {String: "", Valid: true}, editable: true, imputation: true, dv: true, validFrom: "2017-12-18T15:14:49.306583Z"},
     {id: 4, variable: "DVHRPNUM", label: {String: "LABELS1", Valid: true}, description: {String: "Mock Data", Valid: true}, type: "double", length: 8, precision: 0, alias: {String: "", Valid: true}, editable: true, imputation: true, dv: true, validFrom: "2019-12-18T15:14:49.306584Z"}
 ];
 
-export const valueLabelsData = [
+const valueLabelsData = [
     {variable: "ILLST17", label_name: "ILLST17", valid_from: "2019-12-10T00:00:00Z", label_value: 1, description: "1 day", last_updated: "2019-12-19T15:54:25.866691Z"},
     {variable: "ILLST17", label_name: "ILLST17", valid_from: "2019-12-10T00:00:00Z", label_value: 2, description: "2 days", last_updated: "2019-12-19T15:54:25.866691Z"},
     {variable: "ILLST17", label_name: "ILLST17", valid_from: "2019-12-10T00:00:00Z", label_value: 3, description: "3 days", last_updated: "2019-12-19T15:54:25.866692Z"},
